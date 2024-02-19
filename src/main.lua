9490d36e6b0ceebf1fa7278dde84a2513b45633e
gabethenotquitedog <98496788+gabeyk9@users.noreply.github.com>
2024-02-19 09:19:05 -0500

    basic movement

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,6 @@
 function love.draw()
-    love.graphics.print("yay, a game! we are basically done!", 400, 300)
+    -- love.graphics.print(scroll_x, 400-scroll_x, 300-scroll_y)
+    for o,obj in pairs(objects) do
+        love.graphics.rectangle("fill",obj.x-scroll_x,obj.y-scroll_y,24,24)
+    end
 end
