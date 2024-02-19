604f764d0e020b91825dcf9f9975beeaf055a5a2
LostMekka <lostmekka@users.noreply.github.com>
2024-02-19 17:59:06 +0100

    add windfield physics

--- src/main.lua
+++ src/main.lua
@@ -7,38 +7,71 @@ function love.draw()
 end
 
 function love.load()
+    wf = require "libs/windfield"
+    world = wf.newWorld()
+    world:addCollisionClass("player")
+    world:addCollisionClass("enemy")
+    world:addCollisionClass("bullet", { ignores = { "bullet" } })
     setup()
 end
 
 function love.update()
     local player = objects[1]
     dt = love.timer.getDelta()
-    if love.keyboard.isScancodeDown("up","w") then
-        player.y = player.y - dt*player_speed
-    end
-    if love.keyboard.isScancodeDown("down","s") then
-        player.y = player.y + dt*player_speed
-    end
-    if love.keyboard.isScancodeDown("left","a") then
-        player.x = player.x - dt*player_speed
-    end
-    if love.keyboard.isScancodeDown("right","d") then
-        player.x = player.x + dt*player_speed
-    end
+    movePlayer()
     if love.keyboard.isScancodeDown("escape") then
         love.event.quit()
     end
     scroll_x = player.x - 400
     scroll_y = player.y - 300
+    world:update(dt)
+end
+
+function movePlayer()
+    player.x, player.y = player.collider:getPosition() -- we should prly use the collider coords directly
+
+    local playerMoveX, playerMoveY = 0, 0
+    if love.keyboard.isScancodeDown("up","w") then
+        playerMoveY = playerMoveY - 1
+    end
+    if love.keyboard.isScancodeDown("down","s") then
+        playerMoveY = playerMoveY + 1
+    end
+    if love.keyboard.isScancodeDown("left","a") then
+        playerMoveX = playerMoveX - 1
+    end
+    if love.keyboard.isScancodeDown("right","d") then
+        playerMoveX = playerMoveX + 1
+    end
+    local d = math.sqrt(playerMoveX ^ 2 + playerMoveY ^ 2)
+    if (d > 0) then
+        player.collider:applyForce(playerMoveX / d * playerMovementForce, playerMoveY / d * playerMovementForce)
+    end
 end
 
 function setup()
-    player_speed = 150
+    playerMovementForce = 1500
+    playerMovementDamping = 5
     objects = {}
     scroll_x = 0
     scroll_y = 0
-    newobj("player",0,0) -- always make sure player is at position 1
+    player = createPlayer(0, 0) -- always make sure player is at position 1
     newobj("test",40,0)
+end
+
+function createPlayer(x, y)
+    local self = {}
+    self.type = "player"
+    self.x = x
+    self.y = y
+    self.alive = true
+    self.debugColor = { 1, 1, 1 }
+    self.collider = world:newCircleCollider(x, y, 12)
+    self.collider:setCollisionClass("player")
+    self.collider:setLinearDamping(playerMovementDamping)
+    self.collider:setObject(self)
+    table.insert(objects, self)
+    return self
 end
 
 function newobj(type,x,y)
