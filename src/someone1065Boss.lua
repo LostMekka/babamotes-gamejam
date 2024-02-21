88a9545fbf9bb22ff530c5092d9386fafde7fb61
someone1065 <151127011+someone1065@users.noreply.github.com>
2024-02-21 13:22:26 +0200

    Update someone1065Boss.lua

--- src/someone1065Boss.lua
+++ src/someone1065Boss.lua
@@ -1,31 +1,44 @@
 require("Hammer")
 require("OrbitThing")
 
-someone1065Boss = {}
+someone1065Boss = {
+    type = "enemy",
+    belongsToPlayer = false,
+    maxHp = 100,
+    radius = 20,
+    alive = true,
+    mass = 10,
+    debugColor = { 0.447, 0.537, 0.855 }
+}
 
 function someone1065Boss:new(startX, startY)
     local object = {}
     setmetatable(object, self)
     self.__index = self
 
-    object.type = "enemy"
-    object.alive = true
-    object.belongsToPlayer = false
-    object.debugColor = { 0.447, 0.537, 0.855 }
-    object.radius = 20
     object.collider = world:newCircleCollider(startX, startY, object.radius)
     object.collider:setCollisionClass("enemy")
     object.collider:setLinearDamping(playerMovementDamping)
+    object.collider:setMass(object.mass)
     object.collider:setObject(object)
 
-    object.shootInterval = 0.5
-    object.shootTimer = 0
+    addHpComponentToEntity(
+            object,
+            object.maxHp,
+            nil,
+            function(self)
+                -- TODO: mark this boss as defeated
+                spawnPortalToHubWorld(self.collider:getPosition())
+            end
+    )
 
     table.insert(objects, object)
     return object
 end
 
 function someone1065Boss:update(dt)
+    if not player.alive then return end
+
     local px, py
     if self.hammer and self.hammer.alive then
         px, py = self.hammer.collider:getPosition()
@@ -33,10 +46,9 @@ function someone1065Boss:update(dt)
         px, py = player.collider:getPosition()
     end
     local x, y = self.collider:getPosition()
-    local speed = 20
+    local speed = 200
     self.collider:applyForce((px - x) * speed, (py - y) * speed)
 
-    self.shootTimer = self.shootTimer + dt
     if not self.hammer or not self.hammer.alive then
         self.hammer = Hammer:new(
                 self,
