23e2615bd9a6ba4bf7f1dcf762abcdb30bc6a687
LostMekka <lostmekka@users.noreply.github.com>
2024-02-25 22:13:27 +0100

    lostmekka minion sprites

--- src/LostMekkaMinion.lua
+++ src/LostMekkaMinion.lua
@@ -17,6 +17,15 @@ local sounds = {
     death = PolyVoiceSound:new("sfx/bat.wav", 0.5),
 }
 
+local sprites = {
+    loadImage("sprites/lostmekka_minion1.png"),
+    loadImage("sprites/lostmekka_minion2.png"),
+    loadImage("sprites/lostmekka_minion3.png"),
+    loadImage("sprites/lostmekka_minion4.png"),
+    loadImage("sprites/lostmekka_minion5.png"),
+    loadImage("sprites/lostmekka_minion6.png"),
+}
+
 local maxMoveTime = 10000
 
 local function random(a, b)
@@ -40,19 +49,39 @@ function LostMekkaMinion:new(startX, startY, onDeath)
 
     addHpComponentToEntity(object, object.maxHp)
     object.moveTime = random(0, maxMoveTime)
+    object.animationTime = random(0, 5.9)
+    object.animationSpeed = 5
+    object.drawScale = object.radius / 32
 
     table.insert(objects, object)
     return object
 end
 
+function LostMekkaMinion:draw()
+    love.graphics.setColor(1, 1, 1)
+    local animationIndex = math.floor(self.animationTime) + 1
+    local sx, sy = self.collider:getPosition()
+    love.graphics.draw(
+            sprites[animationIndex],
+            sx,
+            sy,
+            0,
+            self.drawScale,
+            self.drawScale,
+            32,
+            32
+    )
+end
+
 function LostMekkaMinion:update(dt)
     self.moveTime = (self.moveTime + dt) % maxMoveTime
+    local f = 1 / self.sizeModifier
+    local t = self.moveTime
+    self.animationTime = (self.animationTime + self.animationSpeed * dt * (1 + love.math.noise((t + 2000) * f * 3.5))) % 6
     if not player or not player.alive then return end
 
     local px, py = player.collider:getPosition()
     local sx, sy = self.collider:getPosition()
-    local f = 1 / self.sizeModifier
-    local t = self.moveTime
     local angleNoise = love.math.noise(t * f)
     local angle = math.atan2(py - sy, px - sx) + ((angleNoise - 0.5) ^ 3) * 180
     local forceNoise = love.math.noise((t + 5000) * f * 2.5)
